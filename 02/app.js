09d1734147bf14082ddb3eb4ae7140c7b86ef648
Wepd <weped_x@163.com>
2021-06-22 21:20:45 +0800

    实战练习完成

--- 02/app.js
+++ 02/app.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const fs = require('fs')
 //加载创建的db封装函数
-const { getDb } = require('./db')
+const { getDb, saveDb } = require('./db')
+
 const app = express()
+
+//解析表单请求体 application/json
+app.use(express.json())
+//application/x-www-form-urlencode
+app.use(express.urlencoded())
+
 
 app.get('/todos', async (req, res) => {
   try{
@@ -65,15 +72,81 @@ app.get('/todos/:id', async (req, res) => {
   // res.send(`get /todos/${req.params.id}`)
 })
 
-app.post('/todos', (reg, res) => {
+app.post('/todos', async (req, res) => {
+  // console.log(req.body)
+  //获取请求体参数
+  try{
+    const todo = req.body
+
+  //数据验证
+  if(!todo.title){
+    return res.status(422).json(
+      {error: "the file title is required" }
+    )
+  }
+  //数据验证通过，把数据存入db中
+  const db = await getDb()
+
+  const lastToDo = db.todos[db.todos.length - 1]
+  todo.id = lastToDo ? lastToDo.id + 1 : 1
+  db.todos.push(todo)
+  //提交数据
+  await saveDb(db)
+  //发送响应
+  res.status(200).json(todo)
   res.send("post todos")
+  }catch(err){
+    res.status(500).json({
+      error: err.message
+    })
+  }
 })
 
-app.patch('/todos', (reg, res) => {
-  res.send("patch todos")
+app.patch('/todos/:id', async (req, res) => {
+  try{
+    //1 獲取表單數據 
+    const todo = req.body 
+    //獲取要修改的任務項
+    const db = await getDb()
+    // db.todos.find(todo => todo.id === req.params.id)
+    // 這樣寫有一個問題就是侯敏獲得是一個字符串二前面的是一個數字，所以要有一個轉換過程
+    const ret = db.todos.find(todo => todo.id === Number.parseInt(req.params.id))
+
+    if(!ret){
+      res.status(404).end()
+    }
+    //合併,將修改過的數據合併到前面查到的結果中
+    Object.assign(ret, todo)
+
+    await saveDb(db)
+
+    res.status(200).json(ret)
+    }catch(err){
+      res.status(500).json({
+        err: err.meaasge
+    })
+  }
+    res.send("patch todos")
 })
 
-app.delete('/todos/:id', (reg, res) => {
+app.delete('/todos/:id', async (req, res) => {
+  try{
+    const todoId = Number.parseInt(req.params.id)
+    const db = await getDb()
+    const index = db.todos.findIndex(todo => todo.id = todoId)
+    if(index === -1){
+      return res.status(404).end()
+    }
+    db.todos.splice(index, 1)
+    await saveDb(db)
+    res.status(200).end()
+  }catch(err){
+    res.status(500).json({
+      error: err.massage
+    })
+  }
+
+
   res.send(`delete todos/${ reg.params.id}`)
 })
 
--- 02/db.js
+++ 02/db.js
@@ -3,6 +3,7 @@ const fs = require("fs")
 //在nodejs的工具模块中提供的一种方法，将异步形式的callback转换为promise的形式
 const { promisify } = require('util')
 const readFile = promisify(fs.readFile)
+const writeFile = promisify(fs.writeFile)
 //现在readFIle是一个提供了promise支持的的操作文件
 
 const path = require('path')
@@ -11,7 +12,14 @@ const path = require('path')
 const dbPath = path.join(__dirname, './db.json')
 
 //将数据文件封装起来
-exports.getDb = async() => {
+exports.getDb = async () => {
   const data = await readFile(dbPath, 'utf8')
   return JSON.parse(data)
+}
+
+//提交文件
+exports.saveDb = async (db) => {
+//db为对象
+const data = JSON.stringify(db)
+await writeFile(dbPath, data)
 }
